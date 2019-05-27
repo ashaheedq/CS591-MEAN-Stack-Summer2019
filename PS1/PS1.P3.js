b0f11766a2755c74beb677e002dbfde5d68cf96c
Abdulshaheed Alqunber <asq@bu.edu>
2019-05-26 23:52:55 -0400

    type in p3

--- PS1/PS1.P3.js
+++ PS1/PS1.P3.js
@@ -1,6 +1,6 @@
 // PS1 Problem 3
 
-// This function that accepts two input parameters: a string, and a function.
+//  This function that accepts two input parameters: a string, and a function.
 // The function then execute the passed function with the passed string and return the result. 
 const doString = (string, lambda) => lambda(string);
 
@@ -16,4 +16,4 @@ let second = doString('supercalifragilisticexpialidocious', str => {
     };
     return strObject;
 })
-console.log(second.o);
+console.log(second);
